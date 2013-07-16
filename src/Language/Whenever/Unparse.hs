84d76204cdaa2735d5e48511e8b934f5ffaf9dad
Michael Tolly <onyxite@gmail.com>
2013-07-16 11:00:22 -0500

    Unparse: removed unnecessary parens and #1

--- src/Language/Whenever/Unparse.hs
+++ src/Language/Whenever/Unparse.hs
@@ -3,13 +3,13 @@ module Language.Whenever.Unparse where
 import Language.Whenever.Base
 import Data.List (intercalate)
 
-expr :: Expr -> String
-expr e = case e of
+expr :: Bool -> Expr -> String
+expr atom e = case e of
   Val v -> case v of
     Str s -> show s
     Int i -> show i
     Bool b -> if b then "true" else "false"
-  Binop op x y -> concat ["(", expr x, " ", operator, " ", expr y, ")"] where
+  Binop op x y -> atomize $ unwords [expr True x, operator, expr True y] where
     operator = case op of
       Plus -> "+"
       Sub -> "-"
@@ -24,18 +24,24 @@ expr e = case e of
       GreaterEqual -> ">="
       Equal -> "=="
       NotEqual -> "!="
-  Not x -> '!' : expr x
+  Not x -> '!' : expr True x
   Read -> "read()"
-  Print x -> concat ["print(", expr x, ")"]
-  N x -> concat ["N(", expr x, ")"]
-  U x -> concat ["U(", expr x, ")"]
-  If c t f -> concat ["(", expr c, " ? ", expr t, " : ", expr f, ")"]
+  Print x -> concat ["print(", expr False x, ")"]
+  N x -> concat ["N(", expr False x, ")"]
+  U x -> concat ["U(", expr False x, ")"]
+  If c t f -> atomize $ unwords
+    [expr True c, "?", expr True t, ":", expr True f]
+  where atomize s = if atom then "(" ++ s ++ ")" else s
 
 stmt :: Stmt -> String
 stmt s = case s of
-  Defer x s' -> concat ["defer (", expr x, ") ", stmt s']
-  Again x s' -> concat ["again (", expr x, ") ", stmt s']
-  Commands cs -> intercalate ", " [ expr x ++ "#" ++ expr y | (x, y) <- cs ] 
+  Defer x s' -> concat ["defer (", expr False x, ") ", stmt s']
+  Again x s' -> concat ["again (", expr False x, ") ", stmt s']
+  Commands cs -> intercalate ", "
+    [ case y of
+        Val (Int 1) -> expr True x
+        _           -> expr True x ++ "#" ++ expr True y
+    | (x, y) <- cs ] 
 
 line :: LineNumber -> Stmt -> String
 line n s = concat [show n, " ", stmt s, ";"]
