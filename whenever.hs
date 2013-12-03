bdec1ba80c46797cc6d0f5e516a0aeb056e8ab6e
Michael Tolly <onyxite@gmail.com>
2013-12-03 13:57:08 -0600

    cleaned up cabal file and executable

--- whenever.hs
+++ whenever.hs
@@ -1,10 +1,22 @@
 module Main where
 
-import Language.Whenever
 import System.IO (hPutStrLn, stderr)
 import System.Environment (getArgs)
+import System.Exit (exitFailure)
+
+import Language.Whenever
 
 main :: IO ()
 main = getArgs >>= \argv -> case argv of
-  [f] -> runFile f
-  _ -> hPutStrLn stderr "Usage: whenever <file-in>"
+  ["-?"] -> printUsage
+  [f]    -> runFile f
+  []     -> getContents >>= runProgram . fromString
+  _      -> printUsage >> exitFailure
+
+printUsage :: IO ()
+printUsage = mapM_ (hPutStrLn stderr)
+  [ "whenever: language by David Morgan-Mar <dmm@dangermouse.net>"
+  , "          interpreter in Haskell by Michael Tolly <miketolly@gmail.com>"
+  , "Usage: whenever file.wr < stdin"
+  , "       whenever < file.wr"
+  ]
